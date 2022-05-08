44e4568b79c520f14bddbaf5b1ed1234ffe2926a
Phuzukuvela Dlamini <Simphiwed@sric.sz>
2022-05-08 14:06:50 +0200

    Password Generator

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,53 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'pw';
+  length = 0;
+  includeLetters = false;
+  includeNumbers = false;
+  useSymbols = false;
+  password = '';
+  
+  onChangeLength(value : string) {
+    const parsedValue = parseInt(value);
+
+    if (!isNaN(parsedValue)) {
+      this.length = parsedValue;
+    }
+  }
+
+  onChangeUseLetters() {
+    this.includeLetters = !this.includeLetters;
+  }
+
+  onChangeUseNumbers() {
+    this.includeNumbers = !this.includeNumbers;
+  }
+
+  onChangeUseSymbols() {
+    this.useSymbols = !this.useSymbols;
+  }
+
+  onButtonClick() {
+    const numbers = '1234567890';
+    const letters = 'abcdefghijklmnopqrstuvwxyz';
+    const symbols = '!@#$%&*()^';
+
+    let validChars = '';
+    if (this.includeLetters) {
+      validChars += letters;
+    }
+    if (this.includeNumbers) {
+      validChars += numbers;
+    }
+    if (this.useSymbols) {
+      validChars += symbols;
+    }
+
+   let generatedPassword = '';
+   for (let i = 0; i <this.length; i++) {
+     const index = Math.floor(Math.random() * validChars.length);
+     generatedPassword += validChars[index];
+   }
+   this.password = generatedPassword;
+  }
 }
